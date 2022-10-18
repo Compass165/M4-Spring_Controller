8b1814a66378bd82e8cc6abdc268ebe3c55cf7bf
Compass <ndphong165@gmail.com>
2022-10-18 12:49:49 +0700

    done yc 9, hien thi voi index

---
+++ Excercise/customer-management-thymleaf/src/main/java/customermanagementthymleaf/controller/CustomerController.java
@@ -0,0 +1,24 @@
+package customermanagementthymleaf.controller;
+
+import customermanagementthymleaf.model.Customer;
+import customermanagementthymleaf.service.CustomerService;
+import customermanagementthymleaf.service.ICustomerService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController {
+    private final ICustomerService customerService = new CustomerService();
+
+    @GetMapping("")
+    public String index(Model model) {
+        List<Customer> customerList = customerService.findAll();
+        model.addAttribute("customers", customerList);
+        return "/index";
+    }
+}
